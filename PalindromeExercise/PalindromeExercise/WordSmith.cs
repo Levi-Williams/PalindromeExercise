646d35711cc82508948d0f530588b10adc0f7e99
Levi Williams <ljw758@hotmail.com>
2023-05-02 01:01:21 -0500

    Completed Exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            char[] chars = word.ToCharArray();
+            Array.Reverse(chars);
+            string reversed = new string(chars);
+
+            if (reversed == word) { return true; }
+
+            else { return false; };
+        }
+
+
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("car", false)]
+        [InlineData("girafarig", true)]
+        [InlineData("mom", true)]
+        [InlineData("memory", false)]
 
+        public void Test1(string word, bool expected)
+        {
+            var test = new WordSmith();
+
+            var actual = test.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);    
         }
     }
 }
